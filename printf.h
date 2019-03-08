d83bebdd1946111906dade2db389e10c0e56bdf1
hstiv <satmak335@gmail.com>
2019-03-09 00:53:12 +0300

    1

---
+++ printf.h
@@ -0,0 +1,12 @@
+#ifndef PRINT_H
+# define PRINT_F
+
+# include <fcntl.h>
+# include <unistd.h>
+# include "libft.h"
+# include <string.h>
+# include <stdlib.h>
+# include <stdio.h>
+# include <stdint.h>
+
+#endif
